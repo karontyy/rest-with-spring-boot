842953714f9902aa42639727e9c6368385c4a84f
bersoncrios <bersondev@gmail.com>
2022-05-27 15:14:09 -0300

    make operations on database

---
+++ src/main/kotlin/br/com/erudio/controller/PersonController.kt
@@ -0,0 +1,54 @@
+package br.com.erudio.controller
+import br.com.erudio.data.vo.v1.PersonVO
+import br.com.erudio.data.vo.v2.PersonVO as PersonVOV2
+import br.com.erudio.services.PersonService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/person")
+class PersonController {
+
+
+    @Autowired
+    private lateinit var service: PersonService
+
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findAll(): List<PersonVO> {
+        return service.findAll()
+    }
+
+    @GetMapping(value = ["/{id}"],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findById(@PathVariable(value="id") id: Long): PersonVO {
+        return service.findById(id)
+    }
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
+                    produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun create(@RequestBody PersonVO: PersonVO): PersonVO {
+        return service.create(PersonVO)
+    }
+
+    @PostMapping(value = ["/v2"],
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun createV2(@RequestBody person: PersonVOV2): PersonVOV2 {
+        return service.createV2(person)
+    }
+
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
+                produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun update(@RequestBody PersonVO: PersonVO): PersonVO {
+        return service.update(PersonVO)
+    }
+
+    @DeleteMapping(value = ["/{id}"],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun delete(@PathVariable(value="id") id: Long): ResponseEntity<*> {
+        service.delete(id)
+        return ResponseEntity.noContent().build<Any >()
+    }
+}
