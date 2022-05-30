94f8446fa42e578c1762758f538a57ce92f8e0a0
bersoncrios <bersondev@gmail.com>
2022-05-30 17:27:43 -0300

    make operations on database

--- src/main/kotlin/br/com/erudio/services/PersonService.kt
+++ src/main/kotlin/br/com/erudio/services/PersonService.kt
@@ -1,13 +1,14 @@
 package br.com.erudio.services
 
+import br.com.erudio.controller.PersonController
 import br.com.erudio.data.vo.v1.PersonVO
-import br.com.erudio.data.vo.v2.PersonVO as PersonVOV2
+import br.com.erudio.exceptions.RequiredObjectIsNullException
 import br.com.erudio.exceptions.ResourceNotFoundException
 import br.com.erudio.mapper.DozerMapper
-import br.com.erudio.mapper.custom.PersonMapper
 import br.com.erudio.model.Person
 import br.com.erudio.repository.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.stereotype.Service
 import java.util.logging.Logger
 
@@ -17,46 +18,53 @@ class PersonService {
     @Autowired
     private lateinit var repository: PersonRepository
 
-    @Autowired
-    private lateinit var mapper: PersonMapper
-
     private val logger = Logger.getLogger(PersonService::class.java.name)
 
     fun findAll(): List<PersonVO> {
         logger.info("Finding all people!")
         val persons = repository.findAll()
-        return DozerMapper.parseListObjects(persons, PersonVO::class.java)
+        val vos = DozerMapper.parseListObjects(persons, PersonVO::class.java)
+        for (person in vos) {
+            val withSelfRel = linkTo(PersonController::class.java).slash(person.key).withSelfRel()
+            person.add(withSelfRel)
+        }
+        return vos
     }
 
     fun findById(id: Long): PersonVO {
-        logger.info("Finding one person!")
+        logger.info("Finding one person with ID $id!")
         var person = repository.findById(id)
             .orElseThrow { ResourceNotFoundException("No records found for this ID!") }
-        return DozerMapper.parseObject(person, PersonVO::class.java)
+        val personVO: PersonVO = DozerMapper.parseObject(person, PersonVO::class.java)
+        val withSelfRel = linkTo(PersonController::class.java).slash(personVO.key).withSelfRel()
+        personVO.add(withSelfRel)
+        return personVO
     }
 
-    fun create(person: PersonVO) : PersonVO{
+    fun create(person: PersonVO?) : PersonVO{
+        if (person == null) throw RequiredObjectIsNullException()
         logger.info("Creating one person with name ${person.firstName}!")
         var entity: Person = DozerMapper.parseObject(person, Person::class.java)
-        return DozerMapper.parseObject(repository.save(entity), PersonVO::class.java)
+        val personVO: PersonVO = DozerMapper.parseObject(repository.save(entity), PersonVO::class.java)
+        val withSelfRel = linkTo(PersonController::class.java).slash(personVO.key).withSelfRel()
+        personVO.add(withSelfRel)
+        return personVO
     }
 
-    fun createV2(person: PersonVOV2) : PersonVOV2{
-        logger.info("Creating one person with name ${person.firstName}!")
-        var entity: Person = mapper.mapVOToEntity(person)
-        return mapper.mapEntityToVO(repository.save(entity))
-    }
-
-    fun update(person: PersonVO) : PersonVO{
-        logger.info("Updating one person with ID ${person.id}!")
-        val entity = repository.findById(person.id)
+    fun update(person: PersonVO?) : PersonVO{
+        if (person == null) throw RequiredObjectIsNullException()
+        logger.info("Updating one person with ID ${person.key}!")
+        val entity = repository.findById(person.key)
             .orElseThrow { ResourceNotFoundException("No records found for this ID!") }
 
         entity.firstName = person.firstName
         entity.lastName = person.lastName
         entity.address = person.address
         entity.gender = person.gender
-        return DozerMapper.parseObject(repository.save(entity), PersonVO::class.java)
+        val personVO: PersonVO = DozerMapper.parseObject(repository.save(entity), PersonVO::class.java)
+        val withSelfRel = linkTo(PersonController::class.java).slash(personVO.key).withSelfRel()
+        personVO.add(withSelfRel)
+        return personVO
     }
 
     fun delete(id: Long) {
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+LOCK TABLES `person` WRITE;
+INSERT INTO `person` VALUES (1,'Palmas - To -Brasil','Guilherme sksks','male','Queiroz'),(2,'Palmas - To -Brasil','Camilla','female','Queiroz'),(3,'Palmas - To -Brasil','Guilherme','male','Berson');
+UNLOCK TABLES;
