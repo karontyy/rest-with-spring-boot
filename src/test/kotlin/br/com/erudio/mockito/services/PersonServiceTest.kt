2eb985badab0c98ccfa2a95d912cf7e286db2bef
bersoncrios <bersondev@gmail.com>
2022-05-31 17:45:38 -0300

    page on result

--- src/test/kotlin/br/com/erudio/mockito/services/PersonServiceTest.kt
+++ src/test/kotlin/br/com/erudio/mockito/services/PersonServiceTest.kt
@@ -1,20 +1,15 @@
-package br.com.erudio.mockito.services
+package br.com.erudio.unittests.mockito.services
 
 import br.com.erudio.exceptions.RequiredObjectIsNullException
 import br.com.erudio.repository.PersonRepository
 import br.com.erudio.services.PersonService
 import br.com.erudio.unittests.mapper.mocks.MockPerson
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
@@ -35,50 +30,6 @@ internal class PersonServiceTest {
     fun setUpMock() {
         inputObject = MockPerson()
         MockitoAnnotations.openMocks(this)
-    }
-
-    @Test
-    fun findAll() {
-        val list = inputObject.mockEntityList()
-        `when`(repository.findAll()).thenReturn(list)
-
-        val persons = service.findAll()
-
-        assertNotNull(persons)
-        assertEquals(14, persons.size)
-
-        val personOne = persons[1]
-
-        assertNotNull(personOne)
-        assertNotNull(personOne.key)
-        assertNotNull(personOne.links)
-        assertTrue(personOne.links.toString().contains("</api/person/v1/1>;rel=\"self\""))
-        assertEquals("Address Test1", personOne.address)
-        assertEquals("First Name Test1", personOne.firstName)
-        assertEquals("Last Name Test1", personOne.lastName)
-        assertEquals("Female", personOne.gender)
-
-        val personFour = persons[4]
-
-        assertNotNull(personFour)
-        assertNotNull(personFour.key)
-        assertNotNull(personFour.links)
-        assertTrue(personFour.links.toString().contains("</api/person/v1/4>;rel=\"self\""))
-        assertEquals("Address Test4", personFour.address)
-        assertEquals("First Name Test4", personFour.firstName)
-        assertEquals("Last Name Test4", personFour.lastName)
-        assertEquals("Male", personFour.gender)
-
-        val personSeven = persons[7]
-
-        assertNotNull(personSeven)
-        assertNotNull(personSeven.key)
-        assertNotNull(personSeven.links)
-        assertTrue(personSeven.links.toString().contains("</api/person/v1/7>;rel=\"self\""))
-        assertEquals("Address Test7", personSeven.address)
-        assertEquals("First Name Test7", personSeven.firstName)
-        assertEquals("Last Name Test7", personSeven.lastName)
-        assertEquals("Female", personSeven.gender)
     }
 
     @Test
